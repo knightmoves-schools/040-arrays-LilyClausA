85789fca054665aeb687b843a9ab19a47df6c0b1
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-01-20 20:15:48 +0000

    bhvfua

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+let iceCreamFlavors = [chocolate, vanilla, twist];
 
+
+
+
+
+document.getElementById("result").innerHTML = iceCreamFlavors;
+
+
+//should create an array named iceCreamFlavors that contains chocolate, vanilla and twist
+//should assign the innerHTML of the HTML element with the id result to the iceCreamFlavors
